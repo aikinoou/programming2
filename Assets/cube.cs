17dd51f8b24355f204a84bf8e81791c5898f3e09
aikinoou <144817574+aikinoou@users.noreply.github.com>
2023-09-22 22:18:20 -0400

    Initial commit

---
+++ Assets/cube.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.Translate(speed, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed, 1, 0);
+    }
+}
